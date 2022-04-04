db461ad2950268bffdeb5aacd03308091ab05c63
wendy1126 <kbbb9278@naver.com>
2022-04-04 20:38:13 +0900

    고양이

---
+++ JavaStudy/백준/p10171.java
@@ -0,0 +1,12 @@
+package Java01;
+
+public class p10171 {
+
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+
+}
