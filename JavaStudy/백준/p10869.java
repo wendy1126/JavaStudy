e3d0b15076fbaade3f4678daa9e488eb78698d19
wendy1126 <kbbb9278@naver.com>
2022-04-04 20:50:17 +0900

    사칙연산

---
+++ JavaStudy/백준/p10869.java
@@ -0,0 +1,20 @@
+package Java01;
+
+import java.util.Scanner;
+
+public class p10869 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);		
+		int x = in.nextInt();
+		int y = in.nextInt();
+		
+		System.out.println(x+y);
+		System.out.println(x-y);
+		System.out.println(x*y);
+		System.out.println(x/y);
+		System.out.println(x%y);
+
+	}
+
+}
