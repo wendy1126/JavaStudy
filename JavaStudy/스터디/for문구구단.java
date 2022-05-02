330e8d6a66a48c0116c1e90f3518e9f603042114
wendy1126 <kbbb9278@naver.com>
2022-05-02 20:55:50 +0900

    자바공부

---
+++ JavaStudy/스터디/for문구구단.java
@@ -0,0 +1,18 @@
+package 스터디;
+
+public class for문구구단 {
+
+	public static void main(String[] args) {
+		
+		for(int i=2; i<=9; i++) {
+			for(int j=1; j<=9; j++) {
+				System.out.println(i+"*"+j+"="+(i*j));
+			}
+			System.out.println();
+		}
+		
+		
+
+	}
+
+}
