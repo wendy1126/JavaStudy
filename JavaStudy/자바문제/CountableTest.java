f59d38e5be814146010259143660640f2f808cf6
WENDY <97227279+wendy1126@users.noreply.github.com>
2022-07-01 17:23:33 +0900

    타입에 따른 다른 메서드 호출 연습2

--- JavaStudy/자바문제/CountableTest.java
+++ JavaStudy/자바문제/CountableTest.java
@@ -7,44 +7,48 @@ interface Countable {
 
 class Bird implements Countable {
 	String name;
+	int num;
 
-	public Bird(String name) {
+	public Bird(String name, int num) {
 		// name을 초기화하는 코드
 		this.name = name;
+		this.num = num;
 
 	}
 
 	// count()와 fly() 메서드를 위한 코드
 	public void count() {
-		System.out.println(name + "가 2마리 있다.");
+		System.out.println(name + "가 " + num + "마리 있다.");
 	}
 
 	public void fly() {
-		System.out.println("2마리 " + name + "가 날아간다.");
+		System.out.println(num + "마리 " + name + "가 날아간다.");
 	}
 }
 
 class Tree implements Countable {
 	String name;
+	int num;
 
-	public Tree(String name) {
+	public Tree(String name, int num) {
 		this.name = name;
+		this.num = num;
 	}
 
 	@Override
 	public void count() {
-		System.out.println(name + "가 5그루 있다.");
+		System.out.println(name + "가 " + num + "그루 있다.");
 	}
 
 	public void ripen() {
-		System.out.println(name + "에 열매가 잘 익었다.");
+		System.out.println(num + "그루 " + name + "에 열매가 잘 익었다.");
 	}
 
 }
 
 public class CountableTest {
 	public static void main(String[] args) {
-		Countable[] m = { new Bird("뻐꾸기"), new Bird("독수리"), new Bird("참새"), new Tree("사과나무"), new Tree("밤나무") };
+		Countable[] m = { new Bird("뻐꾸기", 5), new Bird("독수리", 2), new Tree("사과나무", 10), new Tree("밤나무", 7) };
 
 		for (Countable e : m)
 			e.count();
